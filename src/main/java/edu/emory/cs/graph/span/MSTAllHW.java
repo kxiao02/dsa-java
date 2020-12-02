9be7d239ee5085b9262290894731d92658c38c70
kxiao02 <70287441+kxiao02@users.noreply.github.com>
2020-12-02 03:12:25 -0500

    hw3

--- src/main/java/edu/emory/cs/graph/span/MSTAllHW.java
+++ src/main/java/edu/emory/cs/graph/span/MSTAllHW.java
@@ -1,3 +1,4 @@
+
 package edu.emory.cs.graph.span;
 
 import edu.emory.cs.graph.Edge;
@@ -10,62 +11,74 @@ public class MSTAllHW implements MSTAll {
     interface OnDeal {
         void deal(Deque<Integer> path);
     }
-    public void combine(int n, int k, OnDeal onDeal) {
+
+    // find all combinations of V-1 edges using dft
+    public void dft(int n, int k, OnDeal onDeal) {
         Deque<Integer> path = new ArrayDeque<>();
-        dfs(n, k, 1, path, onDeal);
+        dftAux(n, k, 1, path, onDeal);
     }
 
-    private void dfs(int n, int k, int index, Deque<Integer> path, OnDeal onDeal) {
+    private void dftAux(int n, int k, int index, Deque<Integer> path, OnDeal onDeal) {
         if (path.size() == k) {
             onDeal.deal(path);
             return;
         }
-
         for (int i = index; i <= n - (k - path.size()) + 1; i++) {
-            path.addLast(i);
-            dfs(n, k, i + 1, path, onDeal);
+            path.add(i);
+            dftAux(n, k, i + 1, path, onDeal);
             path.removeLast();
         }
     }
 
+    private boolean isValidST(Graph graph) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        Set<Integer> visited = new HashSet<>();
+
+        queue.add(0);
+        while (!queue.isEmpty()) {
+            int target = queue.poll();
+            visited.add(target);
+            for (Edge edge : graph.getIncomingEdges(target)) {
+                if (!visited.contains(edge.getSource())) queue.add(edge.getSource());
+            }
+        }
+        return visited.size() == graph.size();
+    }
+
+    // randomly pick V-1 edges from E edges
+    // check if the V-1 edges makes a valid ST
+    // if valid ST, check total weight
+    // if equal, count++;
     @Override
     public List<SpanningTree> getMinimumSpanningTrees(Graph graph) {
         MSTPrim mstPrim = new MSTPrim();
         SpanningTree spanningTree = mstPrim.getMinimumSpanningTree(graph);
-
-        double miniSpanningTreeTotalWeight = spanningTree.getTotalWeight();
-
+        // get the minimum total weight with Prim/Kruskal
+        double MSTTotalWeight = spanningTree.getTotalWeight();
+        // get all edges pointing from small to large as the undirected edges
         List<Edge> allEdges = new ArrayList<>();
         for (Edge edge : graph.getAllEdges()) {
-            if (edge.getSource() > edge.getTarget())
-                continue;
+            if (edge.getSource() > edge.getTarget()) continue;
             allEdges.add(edge);
         }
 
-        Set<Integer> nodeSet = new HashSet<>();
         List<SpanningTree> result = new ArrayList<>();
 
-        combine(allEdges.size(), graph.size() - 1, path -> {
-            List<Edge> edges = new ArrayList<>();
-            for (int index : path) {
-                edges.add(allEdges.get(index - 1));
-            }
-
+        dft(allEdges.size(), graph.size() - 1, path -> {
+            // check if ST is valid
             double weights = 0;
-            nodeSet.clear();
-            for(Edge edge : edges) {
-                nodeSet.add(edge.getSource());
-                nodeSet.add(edge.getTarget());
-                weights += edge.getWeight();
-                if (weights > miniSpanningTreeTotalWeight)
-                    break;
+            Graph tmpGraph = new Graph(graph.size());
+            for (int index : path) {
+                tmpGraph.setUndirectedEdge(allEdges.get(index - 1).getSource(),
+                        allEdges.get(index - 1).getTarget(), allEdges.get(index - 1).getWeight());
+                weights += allEdges.get(index - 1).getWeight();
+                if (weights > MSTTotalWeight) break;
             }
 
-            if (nodeSet.size() == graph.size() && weights == miniSpanningTreeTotalWeight) {
+            // if valid ST && total weight == MSTTotalWeight, add the ST to result
+            if (isValidST(tmpGraph) && weights == MSTTotalWeight) {
                 SpanningTree st = new SpanningTree();
-                for(Edge edge : edges) {
-                    st.addEdge(edge);
-                }
+                for (int index : path) st.addEdge(allEdges.get(index - 1));
                 result.add(st);
             }
         });
